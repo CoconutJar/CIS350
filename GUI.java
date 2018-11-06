6deb96bc4d29159d6d132922875299f4e0041bca
CoconutJar <thompbre@mail.gvsu.edu>
2018-11-06 00:07:34 -0500

    Comments
    // descritpions

--- GUI.java
+++ GUI.java
@@ -17,6 +17,11 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+/*******************************************************************************************
+ * 
+ * Chat+ interface.
+ * 
+ ******************************************************************************************/
 public class GUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
@@ -31,6 +36,11 @@ public class GUI extends JFrame implements ActionListener {
 	boolean loggedIn;
 	DataOutputStream dos;
 
+	/****
+	 * 
+	 * Starts up the GUI
+	 * 
+	 ****/
 	public static void main(String args[]) {
 		GUI gui = new GUI();
 		gui.setTitle("Chat Screen");
@@ -38,6 +48,11 @@ public class GUI extends JFrame implements ActionListener {
 		gui.setVisible(true);
 	}
 
+	/****
+	 * 
+	 * Sets up the User Interface.
+	 * 
+	 ****/
 	public GUI() {
 
 		setLayout(new GridBagLayout());
@@ -96,11 +111,19 @@ public class GUI extends JFrame implements ActionListener {
 		connect.addActionListener(this);
 	}
 
+	/****
+	 * 
+	 * When a user clicks a button the actionPerformed method is called with a
+	 * parameter of the component clicked.
+	 * 
+	 ****/
 	public void actionPerformed(ActionEvent e) {
 		JComponent buttonPressed = (JComponent) e.getSource();
 
+		// Grabs the text from a textfield and calls sendMessage.
 		if (buttonPressed == send) {
 
+			// holds message to send to server.
 			String msg = sent.getText();
 			try {
 				sendMessage(msg);
@@ -110,25 +133,29 @@ public class GUI extends JFrame implements ActionListener {
 
 		}
 
-		else if (buttonPressed == iPaddress) {
-
-			displayIPaddress();
-
-		}
-
+		// Try to form a connection using the information in the IPaddress text field.
 		else if (buttonPressed == connect) {
 			try {
 				makeConnection(ipaddress.getText());
 			} catch (IOException e1) {
+
 			}
 
+			// Responsible for reading in any data from the server input stream.
+			// Adds any text received to the chatText box.
 			Thread recieveMessages = new Thread(new Runnable() {
 				@Override
 				public void run() {
+
+					// Will hold all messages received from server.
 					String chatText = "";
+
+					// If the client is loggedOff they wont receive any messages.
 					while (loggedIn) {
+
+						// If the socket is still open.
+						// Read the message sent to this client.
 						try {
-							// read the message sent to this client
 							if (!s.isClosed()) {
 								while (dis.available() > 0) {
 									String msg = dis.readUTF();
@@ -144,45 +171,69 @@ public class GUI extends JFrame implements ActionListener {
 				}
 			});
 
+			// Starts the thread.
 			recieveMessages.start();
 		}
 	}
 
-	private void displayIPaddress() {
-
-	}
-
+	/****
+	 * 
+	 * Forms the TCP socket connection to the server in order to receive and send
+	 * messages.
+	 * 
+	 ****/
 	public void makeConnection(String Name) throws IOException {
 
+		// IP of the server to connect to.
 		InetAddress ip = InetAddress.getByName("localhost");
 
+		// Connection to server.
 		s = new Socket(ip, 3158);
 
+		// Set up input and output stream to send and receive messages.
 		dis = new DataInputStream(s.getInputStream());
 		dos = new DataOutputStream(s.getOutputStream());
 
+		// Sets logged in status to true.
 		loggedIn = true;
 
+		// Sends the UserName of the client to the server.
 		dos.writeUTF(Name);
 
+		// Disabled the connect button and field so you can connect again.
 		ipaddress.setEnabled(false);
 		connect.setEnabled(false);
+
+		// Allows clients to now send messages.
 		sent.setEnabled(true);
 		send.setEnabled(true);
 
 	}
 
+	/****
+	 * 
+	 * Sends the messages to the server using the output stream. If the message is
+	 * 'QUIT' the client disconnects from the server.
+	 * 
+	 ****/
 	public void sendMessage(String message) throws IOException {
 
+		// Send the message using TCP.
 		dos.writeUTF(message);
 
 		if (message.equals("QUIT")) {
+
+			// Closes the connection.
 			s.close();
 			loggedIn = false;
-			connect.setEnabled(true);
-			ipaddress.setEnabled(true);
+
+			// Disables sending messages.
 			send.setEnabled(false);
 			sent.setEnabled(false);
+
+			// Enables connecting to a server.
+			connect.setEnabled(true);
+			ipaddress.setEnabled(true);
 		}
 
 	}
